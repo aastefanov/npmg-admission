3de91f067aa3233e2d82d93d58a5ac39699e7554
Dimitar Vouldjeff <vouldjeff@gmail.com>
2011-03-08 16:33:39 -0800

    fixed locale

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,8 @@ module Admission
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :bg
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.locale = :bg
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
