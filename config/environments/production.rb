12497292ca79342c30dae94219bc4ec3a6b9c11e
Dimitar Vouldjeff <vouldjeff@gmail.com>
2012-05-23 10:00:50 -0700

    minor prod fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,12 +48,13 @@ Admission::Application.configure do
   config.active_support.deprecation = :notify
 
   # Compress JavaScripts and CSS
-config.assets.compress = true
+  config.assets.compress = true
 
-# Don't fallback to assets pipeline if a precompiled asset is missed
-config.assets.compile = false
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
 
-# Generate digests for assets URLs
-config.assets.digest = true
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
+  config.assets.precompile += ['rails_admin/imports.js', 'screen.css']
 end
