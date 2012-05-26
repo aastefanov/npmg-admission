cf21ee828b2fa08971ad1cd2251aad661f36e0d4
Dimitar Vouldjeff <vouldjeff@gmail.com>
2012-05-26 07:47:30 -0700

    image validation

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,5 +5,8 @@ class Asset < ActiveRecord::Base
   attr_accessor :delete_file, :_destroy
   before_validation { self.file.clear if self.delete_file == '1' || _destroy.to_i == 1 }
 
-  validates_presence_of :description, :file
+  validates_presence_of :description
+  validates_attachment :file, :presence => true,
+    :content_type => { :content_type => /image/ },
+    :size => { :in => 0..5.megabytes }
 end
